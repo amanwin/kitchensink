386c54acc277a158f43eeeae8a01b53d33d330dc
Amandeep Singh Chhatwal <aman21singh@gmail.com>
2024-11-19 13:09:08 +0530

    Create ResourceNotFoundException.java

---
+++ src/main/java/com/mongodb/kitchensink/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mongodb.kitchensink.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
